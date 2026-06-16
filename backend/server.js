f4d1706f8dee8263426acf42915a321eb514a9b7
dinhanh0 <anhcongdinh0711@gmail.com>
2026-06-16 14:03:28 -0400

    built full basic CRUD backed

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,9 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
-let tickets = []
+let tickets = [];
 let nextId = 1;
+
 // Enable JSON parsing middleware (useful if you expand the API later)
 app.use(express.json());
 
@@ -15,30 +16,101 @@ app.get("/", (req, res) => {
 
 // GET retrieve the data
 app.get('/api/tickets', (req, res) => {
-    res.send(tickets)
+    res.json(tickets)
+});
+
+app.get('/api/tickets/:id', (req,res) => {
+    const urlId = Number(req.params.id);
+
+    const ticket = tickets.find(t => t.id === urlId); //returns the ticket id if found and 'undefined' if doesn't
+
+    if(!ticket){
+        res.status(404).json({ error: "Ticket not found" }) 
+
+    } else {
+        res.json(ticket)
+    }
+
 });
 
 // POST sends the data
 app.post('/api/tickets', (req, res) => {
 
-    const { title, description, priority } = req.body;
+    const { name, email, title, category, description, priority } = req.body;  //'req' means request
+
+    if (!name || !email || !title || !category || !description){
+        return res.status(400).json({message: "Missing name, email, title, category or description fields, try again"})
+    }
+
+    
 
     const newTicket = {
         id: nextId++, 
+        name,
+        email,
         title,
+        category,
         description,
-        priority: priority || 'low',
+        priority: priority ?? 'low',
         status: 'open',
-        createdAt: new Date()
+        createdAt: new Date(),
+        updatedAt: new Date()
     }
 
     tickets.push(newTicket);
 
-    const newTicket1 = {id: 1, title: "test", description: "this is a test", priority, status, createdAt}
-
     res.json(newTicket)
 });
 
+
+app.put('/api/tickets/:id', (req,res) => {
+
+    const urlId = Number(req.params.id);
+
+    const ticketIndex = tickets.findIndex(t => t.id === urlId); // findIndex returns the array index if found, or -1 if not found
+
+    console.log("ticketIndex:", ticketIndex);
+
+    if(ticketIndex === -1){
+        return res.status(404).json({ error: "Ticket not found" }) 
+
+    }
+
+    const oldTicket = tickets[ticketIndex]
+
+    const updatedTicket = {
+        ...oldTicket,
+        title: req.body.title ?? oldTicket.title,
+        category: req.body.category ?? oldTicket.category,
+        description: req.body.description ?? oldTicket.description,
+        priority: req.body.priority ?? oldTicket.priority,
+        status: req.body.status ?? oldTicket.status,
+        updatedAt: new Date()
+    }
+
+    tickets[ticketIndex] = updatedTicket;
+
+    res.json(updatedTicket);
+});
+
+app.delete('/api/tickets/:id', (req,res) =>{
+    const urlId = Number(req.params.id);
+    const ticketIndex = tickets.findIndex(t => t.id === urlId);
+
+    if (ticketIndex === -1){
+        return res.status(404).json({error: "Ticket not found"}); 
+    }
+
+    const deletedTicketArray = tickets.splice(ticketIndex,1);
+    const deletedTicket = deletedTicketArray[0];
+
+    res.json({
+        message:"The following ticket was deleted successfully",
+        deletedTicket: deletedTicket
+
+    })
+    
+})
 
 
 app.listen(port, () =>{
